c19d60e83dfba905e21f6423fce2cc641ba5be35
Keegan Leary <leary.keegan@gmail.com>
2019-11-09 11:49:10 -0800

    User can login and worked on the bottom message bar.

--- application.py
+++ application.py
@@ -1,11 +1,39 @@
 import os
 
-from flask import Flask, render_template
+from flask_login import LoginManager, login_user, current_user, UserMixin
+from flask import Flask, render_template, request, abort, redirect
 from flask_socketio import SocketIO, emit
 
+allowed_users = ['foo', 'bar', 'Keegan']
+
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+# app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = 'abcdefghijkl'
+
+login_manager = LoginManager(app)
 socketio = SocketIO(app)
+
+
+@login_manager.user_loader
+def user_loader(id):
+    return User(id)
+
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        if username not in allowed_users:
+            abort(401)
+        login_user(User(username))
+        return redirect("/")
+    else:
+        return render_template("login.html")
 
 
 @app.route("/")
@@ -15,13 +43,17 @@ def index():
 
 @socketio.on('user_connects')
 def handle_user_connects(data):
-    print('received message: ' + data['message'])
-    emit('connection_response', data, broadcast=True)
+    if current_user.is_anonymous:
+        return False
+    print(data['message'])
+    data['message'] = current_user.get_id() + " has connected."
+    emit('message_update', data, broadcast=True)
 
 
 @socketio.on('message_send')
 def handle_message_send(data):
     print('Send button was clicked!')
+    data['message'] = current_user.get_id() + ": " + data['message']
     emit('message_update', data, broadcast=True)
 
 
